ec0ec8d8fb67149455ae54d206e5eaaec185705d
Goh Shang Shein <VegeGrinder@gmail.com>
2024-03-12 21:25:40 +0800

    Use tiny-slider for promotion section

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/css/app.css')
     <title>Guilt Free Society</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/tiny-slider.css">
 </head>
 
 <body>
@@ -13,38 +14,28 @@
         <div class="h-3/4 bg-black flex justify-center">
             <img class="my-auto h-1/2 max-h-[400px]" src="{{ url('/images/gfs_main_logo.jpg') }}" alt="gfs_main_logo">
         </div>
-        <div class="h-1/4 flex overflow-x-auto" id="promoSection">
-            <img class="h-full w-full" src="{{ url('/images/monsta_promo.png') }}" alt="monsta_promo">
-            <img class="h-full w-full" src="{{ url('/images/atome_banner.png') }}" alt="atome_banner">
+        <div class="h-1/4 flex overflow-x-auto" id="carousel">
+                <img src="{{ url('/images/monsta_promo.png') }}" alt="monsta_promo">
+                <img src="{{ url('/images/atome_banner.png') }}" alt="atome_banner">
         </div>
     </div>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
     <script>
-        const promoSection = document.getElementById('promoSection');
-        const promoSectionWidth = promoSection.scrollWidth;
-
-        window.addEventListener('load', () => {
-            self.setInterval(scrollRight, 15);
+        var slider = tns({
+            container: '#carousel',
+            items: 1,
+            controls: false,
+            nav: false,
+            autoplay: true,
+            autoplayTimeout: 3500,
+            autoplayButtonOutput: false,
+            responsive: {
+                768: {
+                    items: 2
+                },
+            }
         });
-
-
-        function isElementInViewport(el) {
-            var rect = el.getBoundingClientRect();
-            return rect.right > 0;
-        }
-
-        async function scrollRight() {
-            const first = document.querySelector('#promoSection img');
-
-            if (!isElementInViewport(first)) {
-                promoSection.appendChild(first);
-                promoSection.scrollTo(promoSection.scrollLeft - first.offsetWidth, 0);
-            }
-
-            if (promoSection.scrollLeft !== promoSectionWidth) {
-                promoSection.scrollTo(promoSection.scrollLeft + 1, 0);
-            }
-        }
     </script>
 </body>
 
