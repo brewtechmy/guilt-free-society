70eac46b8adc887aeb0dd43a6e1eb84cf32702ab
Foo Kai Wen <kelvin_foo_13vin@hotmail.com>
2024-03-13 09:28:13 +0800

    removed h-screen to fill all bg colour and inverted colour on click nav item

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -12,16 +12,17 @@
         <nav>
             <div class="text-white flex justify-left">
                 <img class="max-h-48" src="https://scontent.fpen1-1.fna.fbcdn.net/v/t39.30808-6/358469989_755855429872996_368311450259519210_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=js2GABIOE9oAX-BZSul&_nc_ht=scontent.fpen1-1.fna&oh=00_AfArZwwIftoR5TJUUo6evdFgGTJOYcnMDdvwlGLYA0Lxxw&oe=65F54508" />
-                <div onclick="location.href='{{ route('byob') }}';" class="flex items-center p-8 hover:cursor-pointer">
+                <div onclick="location.href='{{ route('byob') }}';" class="flex items-center p-8 hover:cursor-pointer {{ request()->is("byob") ? "bg-[#e2fbe2] text-black" : "" }}">
+
                     Build Your Own Bowl
                 </div>
                 <div class="flex items-center p-8 hover:cursor-pointer">
                     Menu
                 </div>
-                <div onclick="location.href='{{ route('story') }}';" class="flex items-center p-8 hover:cursor-pointer">
+                <div onclick="location.href='{{ route('story') }}';" class="flex items-center p-8 hover:cursor-pointer {{ request()->is("story") ? "bg-[#e2fbe2] text-black" : "" }}">
                     Our Story
                 </div>
-                <div onclick="location.href='{{ route('contact-us') }}';" class="flex items-center p-8 hover:cursor-pointer">
+                <div onclick="location.href='{{ route('contact-us') }}';" class="flex items-center p-8 hover:cursor-pointer {{ request()->is("contact-us") ? "bg-[#e2fbe2] text-black" : "" }}">
                     Contact Us
                 </div>
                 <div class="pl-4 grow">
@@ -32,7 +33,7 @@
         </nav>
     </header>
 
-    <main class="bg-[#e2fbe2] h-screen">
+    <main class="bg-[#e2fbe2]">
         @yield('content')
     </main>
 
