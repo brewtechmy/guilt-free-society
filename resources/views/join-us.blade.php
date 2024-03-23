a4737dae6eb3afc42823cfbb8fa613f092b08c83
Goh Shang Shein <VegeGrinder@gmail.com>
2024-03-23 19:31:30 +0800

    Standardize page main title and padding-x

--- resources/views/join-us.blade.php
+++ resources/views/join-us.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts.menu')
 @section('title', 'GFS - Join Us')
 @section('content')
-    <div class="px-14 flex flex-row">
-        <div class="basis-4/12 py-4 h-[calc(100dvh-192px)] flex flex-col">
-            <span class="flex-none text-4xl font-semibold">Partner With Us</span>
-            <div class="grow flex flex-col justify-center">
-                <img class="rounded-full border-8 border-black aspect-square min-w-72 max-w-72 mx-auto"
-                    src="https://scontent.fkul4-4.fna.fbcdn.net/v/t39.30808-6/398490368_17998655525246821_8464446388800192676_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_ohc=hUGGBA4-i7YAX86dJve&_nc_ht=scontent.fkul4-4.fna&oh=00_AfBHjf4WSdzo0D3J-SG9-olNoepxtAsPcTFWd0DKJYDeTA&oe=65F6C480" />
-            </div>
+    <div class="px-14 relative">
+        <div class="flex flex-row pt-5">
+            <span class="text-6xl font-semibold">Partner With Us</span>
         </div>
-        <div class="basis-8/12">
-            <div class="py-4 flex flex-col space-y-3">
+        <div class="px-14 flex flex-row">
+            <div class="basis-4/12 py-4 h-[calc(100dvh-192px)] flex flex-col">
+                <div class="grow flex flex-col justify-center">
+                    <img class="rounded-full border-8 border-black aspect-square min-w-72 max-w-72 mx-auto"
+                        src="https://placehold.co/600x600" />
+                </div>
+            </div>
+            <div class="basis-8/12">
+                <div class="py-4 flex flex-col space-y-3">
 
+                </div>
             </div>
         </div>
     </div>
