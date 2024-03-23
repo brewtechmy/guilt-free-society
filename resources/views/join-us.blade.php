37fcf87dd72e2ec46cda357e6012cf3c3f1cb2d8
Goh Shang Shein <VegeGrinder@gmail.com>
2024-03-23 20:04:57 +0800

    Complete frontend for Join Us page (desktop view)

--- resources/views/join-us.blade.php
+++ resources/views/join-us.blade.php
@@ -5,16 +5,56 @@
         <div class="flex flex-row pt-5">
             <span class="text-6xl font-semibold">Partner With Us</span>
         </div>
-        <div class="px-14 flex flex-row">
-            <div class="basis-4/12 py-4 h-[calc(100dvh-192px)] flex flex-col">
+        <div class="px-14 grid grid-cols-5 gap-8">
+            <div class="col-span-2 flex flex-col">
                 <div class="grow flex flex-col justify-center">
                     <img class="rounded-full border-8 border-black aspect-square min-w-72 max-w-72 mx-auto"
                         src="https://placehold.co/600x600" />
                 </div>
             </div>
-            <div class="basis-8/12">
-                <div class="py-4 flex flex-col space-y-3">
+            <div class="col-span-3 overflow-y-auto">
+                <div class="py-4 flex flex-row gap-5">
+                    <div class="basis-2/6 self-center">
+                        <img class="rounded-full border-8 border-black aspect-square" src="https://placehold.co/600x600" />
+                    </div>
+                    <div class="flex flex-col basis-4/6 justify-center">
+                        <div class="text-2xl items-center font-bold">Franchising</div>
+                        <span class="text-md items-center mt-2">Interested in opening YOUR OWN GFS OUTLET?</span>
+                    </div>
+                </div>
 
+                <div class="py-4 flex flex-row gap-5">
+                    <div class="basis-2/6 self-center">
+                        <img class="rounded-full border-8 border-black aspect-square" src="https://placehold.co/600x600" />
+                    </div>
+                    <div class="flex flex-col basis-4/6 justify-center">
+                        <div class="text-2xl items-center font-bold">Physical & Mental Health</div>
+                        <span class="text-md items-center mt-2">Are you a personal trainer, nutritionist, dietitian, Zumba instructor?</span>
+                    </div>
+                </div>
+
+                <div class="py-4 flex flex-row gap-5">
+                    <div class="basis-2/6 self-center">
+                        <img class="rounded-full border-8 border-black aspect-square" src="https://placehold.co/600x600" />
+                    </div>
+                    <div class="flex flex-col basis-4/6 justify-center">
+                        <div class="text-2xl items-center font-bold">Mental Health</div>
+                        <span class="text-md items-center mt-2">Or are you a psychologist/psychology graduate, counsellor?</span>
+                    </div>
+                </div>
+
+                <div class="py-4 flex flex-row gap-5">
+                    <div class="basis-2/6 self-center">
+                        <img class="rounded-full border-8 border-black aspect-square" src="https://placehold.co/600x600" />
+                    </div>
+                    <div class="flex flex-col basis-4/6 justify-center">
+                        <div class="text-2xl items-center font-bold">Creative Ideas</div>
+                        <span class="text-md items-center mt-2">Do you have creative ideas?</span>
+                        <span class="text-md items-center mt-2">
+                            Whatsapp us @ 016-451 3003<br>
+                            E-mail us @ guiltfreesociety@gmail.com
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
