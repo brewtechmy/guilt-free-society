17409814f741a6cd427303af37955a165421e3d4
Foo Kai Wen <kelvin_foo_13vin@hotmail.com>
2024-04-20 13:37:57 +0800

    added some default values for sections migration script

--- database/migrations/2024_03_27_045108_create_sections_table.php
+++ database/migrations/2024_03_27_045108_create_sections_table.php
@@ -23,31 +23,31 @@ return new class extends Migration
 
         Section::insert([
             'key' => 'number_bowl_sold',
-            'value' => '0',
+            'value' => '4790',
             'input_type' => 'number'
         ]);
 
         Section::insert([
             'key' => 'our_vision_text',
-            'value' => '',
+            'value' => '<p>Our local community sees a significant improvement in their physical, mental health &amp; well-being.</p>',
             'input_type' => 'ckeditor'
         ]);
 
         Section::insert([
             'key' => 'our_values_text',
-            'value' => '',
+            'value' => '<p>We believe in one thing - <strong>Eat Well, Live Better</strong></p><p>So, we promise to bring you only <strong>Food Made Real</strong>.</p><p>Every day, we strive to curate healthy bowls which are delicious and nutritious to help improve your overall health and well-being.</p>',
             'input_type' => 'ckeditor'
         ]);
 
         Section::insert([
             'key' => 'our_mission_text',
-            'value' => '',
+            'value' => '<p>To establish a one-stop centre in your neighbourhood that provides healthy fast food, fitness training and mental health services.</p>',
             'input_type' => 'ckeditor'
         ]);
 
         Section::insert([
             'key' => 'help_us_improve_link',
-            'value' => '',
+            'value' => 'https://docs.google.com/forms/d/e/1FAIpQLSdYlxIwrctHcxvs1Kwnc8FqHxQoe1CcZa26Q4aENzCFZ8-n0w/viewform',
             'input_type' => 'text'
         ]);
     }
