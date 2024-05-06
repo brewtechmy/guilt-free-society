d08b0514c8c51716bd9b7eb003dc3960ef982954
Eric Tan <erictan120796@gmail.com>
2024-05-06 22:35:13 +0800

    update join us seeder

--- database/seeders/JoinUsPageSeeder.php
+++ database/seeders/JoinUsPageSeeder.php
@@ -16,30 +16,46 @@ class JoinUsPageSeeder extends Seeder
         $joinUs = [
             [
                 'id'    => 1,
-                'title' => "Franchising",
-                'position' => 1,
-                'description' => '<p>Interested in opening YOUR OWN GFS OUTLET?</p>',
+                'title' => null,
+                'position' => null,
+                'description' => null,
+                'is_main' => true,
+                'image_path' => 'Partner-with-us.png'
             ],
             [
                 'id'    => 2,
-                'title' => "Physical & Mental Health",
-                'position' => 2,
-                'description' => '<p>Are you a personal trainer, nutritionist, dietitian, Zumba instructor?</p>',
+                'title' => "Franchising",
+                'position' => 1,
+                'description' => '<p>Interested in opening YOUR OWN GFS OUTLET?</p>',
+                'image_path' => 'Franchising.png'
             ],
             [
                 'id'    => 3,
-                'title' => "Mental Health",
-                'position' => 3,
-                'description' => '<p>Or are you a psychologist/psychology graduate, counsellor?</p>',
+                'title' => "Physical & Mental Health",
+                'position' => 2,
+                'description' => '<p>Are you a personal trainer, nutritionist, dietitian, Zumba instructor?</p>',
+                'image_path' => 'Personal-trainer.jpg'
             ],
             [
                 'id'    => 4,
+                'title' => "Mental Health",
+                'position' => 3,
+                'description' => '<p>Or are you a psychologist/psychology graduate, counsellor?</p>',
+                'image_path' => 'Psychologist-psychology.jpg'
+            ],
+            [
+                'id'    => 5,
                 'title' => "Creative Ideas",
                 'position' => 4,
                 'description' => '<p>Do you have creative ideas?</p><p><a href="https://wa.me/60164513003"><i>Whatsapp us @ 016-451 3003</i></a><br><a href="mailto:guiltfreesociety@gmail.com"><i>E-mail us @ guiltfreesociety@gmail.com</i></a></p>',
+                'image_path' => 'Creative-bowl.jpg'
             ],
         ];
 
-        JoinUsPage::insert($joinUs);
+        foreach ($joinUs as $data) {
+            $imagePath = array_pop($data);
+            $JoinUsPage = JoinUsPage::create($data);
+            $JoinUsPage->addMedia("database/seeders/join_us_images/".$imagePath)->preservingOriginal()->toMediaCollection('photo');
+        }
     }
 }
