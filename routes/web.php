e83b2278a6416b092e50998b59397df876228e01
Foo Kai Wen <kelvin_foo_13vin@hotmail.com>
2024-05-21 11:26:28 +0800

    default base route to redirect to welcome page

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/login');
+Route::redirect('/', '/welcome');
 Route::get('/home', function () {
     if (session('status')) {
         return redirect()->route('admin.home')->with('status', session('status'));
